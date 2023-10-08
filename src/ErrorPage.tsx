78cc3895c91532ed12719983ee6b9f2fa5d27fbe
Bakhodirbek Saldjanov <bakhodirbek.saldjanov@unicast.ne.jp>
2023-10-08 22:49:01 +0900

    update error message

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -6,7 +6,7 @@ export const ErrorPage = () => {
     }, []);
     return (
         <div className="Error">
-            Ma'lumot topilmadi  111
+            Ma'lumot topilmadi  - Zvonyu dedu v immigrashku
         </div>
     )
 }
